9cc6e2519b9eb6d9aadc95879fcb6caf37ac3e33
Kevin Hyland <khy@me.com>
2013-07-29 22:58:38 -0400

    Refactor GiltRetriever
    * factor out generic URL downloading into UrlRetriever
    * add a bit of logging

--- app/lib/Retriever.scala
+++ app/lib/Retriever.scala
@@ -6,6 +6,7 @@ import java.io.ByteArrayInputStream
 import javax.imageio.ImageIO
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.Logger
 
 trait Retriever {
 
@@ -13,20 +14,32 @@ trait Retriever {
 
 }
 
-class GiltRetriever extends Retriever {
+trait UrlRetriever extends Retriever {
+
+  def urlForKey(key: String): String
 
   def retrieve(key: String) = {
-    val path = (("0" * 16) + key).takeRight(16).grouped(4).take(3).mkString("/")
-    val url = "http://cdn1.giltcdn.com/images/share/uploads/%s/%s/orig.jpg".format(path, key)
+    val url = urlForKey(key)
     WS.url(url).get().map { response =>
       if (response.status == 200) {
+        Logger.info("Download from %s succeeded!".format(url))
         val bytes = response.getAHCResponse.getResponseBodyAsBytes
         val stream = new ByteArrayInputStream(bytes)
         Some(ImageIO.read(stream))
       } else {
+        Logger.info("Download from %s failed: %s".format(url, response.statusText))
         None
       }
     }
+  }
+
+}
+
+class GiltRetriever extends UrlRetriever {
+
+  def urlForKey(key: String) = {
+    val path = (("0" * 16) + key).takeRight(16).grouped(4).take(3).mkString("/")
+    "http://cdn1.giltcdn.com/images/share/uploads/%s/%s/orig.jpg".format(path, key)
   }
 
 }
