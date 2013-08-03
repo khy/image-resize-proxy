6e578a2200ea2be950ed0f5d967a75aa0860e9bb
Kevin Hyland <khy@me.com>
2013-08-03 18:47:24 -0400

    Make LoadSimulation more realistic

--- gatling/LoadSimulation.scala
+++ gatling/LoadSimulation.scala
@@ -25,16 +25,54 @@ class LoadSimulation extends Simulation {
     ids(Random.nextInt(ids.length))
   }
 
+  def randomDimension() = {
+    val dimensions = Seq(
+      Some("100"),
+      Some("200"),
+      Some("300"),
+      Some("400"),
+      Some("500"),
+      Some("600"),
+      Some("700"),
+      None
+    )
+    dimensions(Random.nextInt(dimensions.length))
+  }
+
+  def randomRequest() = {
+    val imageId = randomImageId()
+    val width = randomDimension()
+    val height = randomDimension()
+    var request = http("Get image: %s".format(imageId)).get("/" + imageId)
+
+    if (width.isDefined) {
+      request = request.queryParam("w", width.get)
+    }
+
+    if (height.isDefined) {
+      request = request.queryParam("h", height.get)
+    }
+
+    request.check(status.is(200))
+  }
+
+  def randomRequestSequence() = {
+    var requestSequence = exec(
+      randomRequest()
+    ).pause(0 milliseconds, 5 milliseconds)
+
+    1 to 100 foreach { _ => 
+      requestSequence = requestSequence.exec(
+        randomRequest()
+      ).pause(0 milliseconds, 5 milliseconds)
+    }
+
+    requestSequence
+  }
+
   val httpConf = http.baseURL("http://localhost:9000")
 
-  val getImageScenario = scenario("Image Endpoint")
-   .during(10) {
-     exec(
-       http("Get Random Image")
-         .get("/" + randomImageId())
-         .check(status.is(200)))
-     .pause(0 milliseconds, 5 milliseconds)
-   }
+  val getImageScenario = scenario("Image Endpoint").during(10) { randomRequestSequence() }
 
   setUp {
     getImageScenario.inject(ramp(100 users) over (10 seconds))
